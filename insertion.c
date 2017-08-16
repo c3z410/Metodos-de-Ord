13a8033e71615b6313b8dec738361a64922da5e1
c3z410 <caioedu1@hotmail.com>
2017-08-16 10:13:35 -0300

    Add files via upload

---
+++ insertion.c
@@ -0,0 +1,32 @@
+void insertionsort(int ar[], int n)
+{
+
+int i, j, key;
+
+for (i = 1; i < n; ++i){
+	key = ar[i];
+
+	for (j = i - 1; j >= 0 && key < ar[j]; --j){
+		ar[j + 1] = ar[j];
+	}
+	ar[j + 1] = key;
+	}
+}
+
+
+void printarray(int ar[], int n)
+{
+   int i;
+   for (i=0; i < n; i++){printf("%d ", ar[i]);}
+   printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 8};
+    printarray(arr, 6);
+    insertionsort(arr, 6);
+    printarray(arr, 6);
+
+    return 0;
+}
---
+++ selection.c
@@ -0,0 +1,37 @@
+void selectionsort(int ar[], int n)
+{
+	int i, j, indice, maior;
+
+	for (i = n - 1; i > 0; --i) {
+
+		maior = ar[0];
+		indice = 0;
+
+		for (j = 1; j <= i; ++j){
+
+			if (ar[j] > maior) {
+				maior = ar[j];
+				indice = j;
+			}
+		}
+		ar[indice] = ar[i];
+		ar[i] = maior;
+	}
+}
+
+void printarray(int ar[], int n)
+{
+   int i;
+   for (i=0; i < n; i++){printf("%d ", ar[i]);}
+   printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 8};
+    printarray(arr, 6);
+    selectionsort(arr, 6);
+    printarray(arr, 6);
+
+    return 0;
+}
