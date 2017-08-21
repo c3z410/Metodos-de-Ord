8a77e204334dffea6204aacf551d88aac9ef09a7
c3z410 <caioedu1@hotmail.com>
2017-08-21 19:28:32 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "selection.h"
+#include "insertion.h"
+#include <stdio.h>
+
+void printarray(int ar[], int n)
+{
+   int i;
+   for (i=0; i < n; i++){printf("%d ", ar[i]);}
+   printf("\n");
+}
+
+int main()
+{
+    int arr[] = {15, 14, 17, 11, 12, 13};
+    int arr2[] = {15, 14, 17, 11, 12, 13};
+
+    printf("Insertion Sort\n");
+
+    printarray(arr, 6);
+    insertionsort(arr, 6);
+    printarray(arr, 6);
+
+    printf("Selection Sort\n");
+
+    printarray(arr2, 6);
+    selectionsort(arr2, 6);
+    printarray(arr2, 6);
+
+    return 0;
+}
---
+++ ordcomp.h
@@ -0,0 +1,7 @@
+#ifndef ORDCOMP_H
+#define ORDCOMP_H
+
+void selectionsort(int ar[], int n);
+void insertionsort(int ar[], int n);
+
+#endif
