241cc2c9fccb5018549f1c74b850b04f0f586b0c
c3z410 <caioedu1@hotmail.com>
2017-08-31 10:28:48 -0300

    Update merges.c

--- comparacao2/merges.c
+++ comparacao2/merges.c
@@ -3,7 +3,6 @@
 
 void merge(int ar1[], int ar2[], int L, int m, int r){
    int i, L2, nElement, posAux;
-
    L2 = m - 1;
    posAux = L;
    nElement = r - l + 1;
@@ -12,7 +11,6 @@ void merge(int ar1[], int ar2[], int L, int m, int r){
          ar2[posAux++] = ar1[L++];
       else
          ar2[posAux++] = ar1[m++];
-
    while(L <= L2)
       ar2[posAux++] = ar1[L++];
    while(m <= r)
@@ -21,12 +19,10 @@ void merge(int ar1[], int ar2[], int L, int m, int r){
    for(i = 0; i < nElement; i++, r--)
       ar1[r] = ar2[r];
 }
-
 void mergesort(int ar[], int L, int r){
      int *aux, m;
      int nElem = r-L+1;
      aux = malloc(nElem*sizeof(int));
-
      if(L < r) {
           m = (L + r)/2;
           mergesort(array, L, m);
