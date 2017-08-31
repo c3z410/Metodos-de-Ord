a20d12e9a3fef1ac9a0c5d32f508512febeb9794
c3z410 <caioedu1@hotmail.com>
2017-08-31 10:53:21 -0300

    Create heaps.c

---
+++ comparacao2/heaps.c
@@ -0,0 +1,28 @@
+#include "heaps.h"
+
+void heapify(int heap[], int ify, int n){
+   int son, aux;
+   aux = heap[ify];
+   while (2*ify + 1 < n) {
+      son = 2*ify + 1;
+      if((son != n-1) && (heap[son+1] > heap[son]))
+         son++;
+      if(aux < heap[son])
+         heap[ify] = heap[son];
+      else
+         break;
+      ify = son;
+   }
+   heap[ify] = aux;
+}
+
+void HeapSort(int ar[], int size){
+   int i, aux;
+   for(i = (size-2)/2; i >= 0; i--)
+      heapify(ar, i, size);
+   for(i = size-1; i >= 1; i--){
+      aux = ar[0];
+      ar[0] = array[i];
+      ar[i] = aux;
+      heapify(ar, 0, i);}
+}
