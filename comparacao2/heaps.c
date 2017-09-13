cc2c716098edf7402f0f912e75918d090f853d11
c3z410 <caioedu1@hotmail.com>
2017-09-13 10:26:07 -0300

    Update heaps.c

--- comparacao2/heaps.c
+++ comparacao2/heaps.c
@@ -22,7 +22,7 @@ void HeapSort(int ar[], int size){
       heapify(ar, i, size);
    for(i = size-1; i >= 1; i--){
       aux = ar[0];
-      ar[0] = array[i];
+      ar[0] = ar[i];
       ar[i] = aux;
       heapify(ar, 0, i);}
 }
