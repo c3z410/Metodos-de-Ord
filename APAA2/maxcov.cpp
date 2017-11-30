a69fbc4140fd35a3d984f46cf614174daebf2b4a
c3z410 <caioedu1@hotmail.com>
2017-11-30 11:24:06 -0300

    Add files via upload

--- APAA2/maxcov.cpp
+++ APAA2/maxcov.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <limits.h>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
 
@@ -15,7 +15,15 @@ struct ponto{
     bool checked;
 };
 
+int media = 0;
+int best = 0;
+
 vector <struct ponto> reader(const char * str, int * nfac, int * range);
+
+bool compare_v(vector<int>& a, vector<int>& b)
+{
+   return (a.size() >= b.size());
+}
 
 void Movvizinhanca(vector <struct ponto> clientes, int facilidades, int range, vector <int> * solution){
     int menor = INT_MAX;
@@ -29,15 +37,51 @@ void Movvizinhanca(vector <struct ponto> clientes, int facilidades, int range, v
         }
     }
 
-    for(int i = 0;i != solution[menorindice].size();i++){
-    clientes[solution[menorindice][i]].checked = false;
-  }
+    /*for(int i = 0;i != solution[menorindice].size();i++){
+      clientes[solution[menorindice][i]].checked = false;
+    }*/
+    /*
+    solution[menorindice].clear();
+    vector <int> org[324];
+
+    for (int k = 0; k != 323; k++){
+      org[k].push_back(k);
+
+      for (int l = k + 1; l != k; l = (l+1)%(clientes.size()-1)){
+
+        float d = sqrt(((clientes[k].x - clientes[l].x)*(clientes[k].x - clientes[l].x)) + ((clientes[k].y - clientes[l].y)*(clientes[k].y - clientes[l].y)));
+        if((d<=range) && !((clientes[k].checked) || (clientes[l].checked))) (org[k]).push_back(l);
+      }
+    }
+
+    sort(begin(org),end(org),compare_v);
+
+    solution[menorindice].push_back(org[0][0]);
+    clientes[org[0][0]].checked = true;
+
+    for(int j = 0; j < clientes.size(); j++){
+      if(!(clientes[j].checked)){
+
+          float dist = sqrt(((clientes[org[0][0]].x - clientes[j].x)*(clientes[org[0][0]].x - clientes[j].x)) + ((clientes[org[0][0]].y - clientes[j].y)*(clientes[org[0][0]].y - clientes[j].y)));
+
+          if(dist <= range){
+              (solution[menorindice]).push_back(j);
+              clientes[j].checked = true;
+          }
+      }
+    }*/
 
     for(int i = ((solution[menorindice])[0] + 1); i != (solution[menorindice])[0]; i = (i+1)%(clientes.size()-1)){
 
         if(!(clientes[i].checked)){
 
           vector <int> key = solution[menorindice];
+
+          for(int u = 0;u != solution[menorindice].size()-1;u++){
+            clientes[solution[menorindice][u]].checked = false;
+          }
+
+          //vector <int> key = solution[menorindice];
 
            solution[menorindice].clear();
 
@@ -57,6 +101,12 @@ void Movvizinhanca(vector <struct ponto> clientes, int facilidades, int range, v
                }
            }
            if (solution[menorindice].size() < key.size()){
+             for(int u = 0;u != solution[menorindice].size()-1;u++){
+               clientes[solution[menorindice][u]].checked = false;
+             }
+             for(int u = 0;u != key.size()-1;u++){
+               clientes[key[u]].checked = true;
+             }
              solution[menorindice].clear();
              solution[menorindice]=key;
            }
@@ -65,78 +115,108 @@ void Movvizinhanca(vector <struct ponto> clientes, int facilidades, int range, v
 }
 
 
+void Solution(vector <struct ponto> clientes, int facilidades, int range, vector <int> * solution){
 
 
- void Solution(vector <struct ponto> clientes, int facilidades, int range, vector <int> * solution){
+   srand(time(NULL));
 
-    srand(time(0));
+   for(int i = 0; i < facilidades; i++){
 
+       vector <int> org[324];
+       for (int k = 0; k != 323; k++){
+         org[k].push_back(k);
+         for (int l = k + 1; l != k; l = (l+1)%(clientes.size()-1)){
+           float d = sqrt(((clientes[k].x - clientes[l].x)*(clientes[k].x - clientes[l].x)) + ((clientes[k].y - clientes[l].y)*(clientes[k].y - clientes[l].y)));
+           if((d<=range) && !((clientes[k].checked) || (clientes[l].checked))) (org[k]).push_back(l);
+         }
+       }
 
-    for(int i = 0; i < facilidades; i++){
-
-        int f = (rand()%(clientes.size()-1));
-
-        while(clientes[f].checked){
-
-            f = (rand()%(clientes.size()-1));
-        }
-
-
-
-        (solution[i]).push_back(f);
-        clientes[f].checked = true;
+       sort(begin(org),end(org),compare_v);
 
 
-        for(int j = 0; j < clientes.size(); j++){
-            if(!(clientes[j].checked)){
+       /*int meme = 0;
+       for (int k = 323; k != 0; k--){
+          if(org[k].size() > 1){
+            meme = k;
+            break;
+          }
+       }*/
 
-                float dist = sqrt(((clientes[f].x - clientes[j].x)*(clientes[f].x - clientes[j].x)) + ((clientes[f].y - clientes[j].y)*(clientes[f].y - clientes[j].y)));
+      //int randomn = rand()%(int)((org[0].size() + org[meme].size())*0.5);
+      float filtro = ((org[0].size() + org[323].size())*0.9);
+      int mimd = 0;
+      for (int k = 323; k != 0; k++){
+         if(org[k].size() >= filtro){
+           mimd = k;
+           break;
+         }
+      }
+      int randomn = rand()%(mimd);
 
-                if(dist <= range){
-                    (solution[i]).push_back(j);
-                    clientes[j].checked = true;
-                }
-            }
-        }
-    }
+      while(clientes[randomn].checked){
 
-
-    int count = 0;
-
-    for(int i = 0; i < facilidades; i++){
-
-        cout << "\nF " << solution[i][0] << "  --- C: ";
-
-        for(int j = 1; j < solution[i].size(); j++){
-            cout<< ((solution[i])[j]) << " ";
-        }
-        count += (solution[i]).size()-1;
-    }
-
-    cout << "\n\n"<< count <<" pontos cobertos\n";
-
-    for(int m = 0 ; m < 5 ; m++){
-      Movvizinhanca(clientes, facilidades, range, solution);}
-
-    cout << "\n\nCom Movimento de Vizinhanca: \n";
-
-    count = 0;
-
-    for(int i = 0; i < facilidades; i++){
-
-        cout << "\nF " << solution[i][0] << "  --- C: ";
-
-        for(int j = 1; j < solution[i].size(); j++){
-            cout<< ((solution[i])[j]) << " ";
-        }
-        count += (solution[i]).size()-1;
-    }
-
-    cout << "\n\n"<< count <<" pontos cobertos :)\n";
-
- }
+          randomn = rand()%(mimd);
+      }
+      //cout << randomn << endl;
+      solution[i].push_back(randomn);
+      //for(int n = 0; n < org[randomn].size(); n++){
+      //  cout << org[randomn][0] << endl;
+        clientes[randomn].checked = true;
+      //}
 
 
+
+       for(int j = 0; j < clientes.size()-1; j++){
+           if(!(clientes[j].checked)){
+
+               float dist = sqrt(((clientes[randomn].x - clientes[j].x)*(clientes[randomn].x - clientes[j].x)) + ((clientes[randomn].y - clientes[j].y)*(clientes[randomn].y - clientes[j].y)));
+
+               if(dist <= range ){
+                   (solution[i]).push_back(j);
+                   clientes[j].checked = true;
+               }
+           }
+       }
+      //for (int z = 0; z != 323; z++) org[z].clear();
+   }
+
+
+   int count = 0;
+
+   for(int i = 0; i < facilidades; i++){
+
+       cout << "\nF " << solution[i][0] << "  --- C: ";
+
+       for(int j = 1; j < solution[i].size(); j++){
+           cout<< ((solution[i])[j]) << " ";
+       }
+       count += (solution[i]).size()-1;
+   }
+
+   cout << "\n\n"<< count <<" pontos cobertos\n";
+
+   //for(int h = 0; h < 5;h++)
+   Movvizinhanca(clientes, facilidades, range, solution);
+
+   cout << "\n\nCom Movimento de Vizinhanca: \n";
+
+   count = 0;
+
+   for(int i = 0; i < facilidades; i++){
+
+       cout << "\nF " << solution[i][0] << "  --- C: ";
+
+       for(int j = 1; j < solution[i].size(); j++){
+           cout<< ((solution[i])[j]) << " ";
+       }
+       count += (solution[i]).size()-1;
+   }
+
+   cout << "\n\n"<< count <<" pontos cobertos \n";
+   if(best < count)best = count;
+   media += count;
+
+}
 
 
 int main(){
@@ -145,9 +225,23 @@ int main(){
     vector <struct ponto> clientes;
     clientes = reader("input.txt", &nfac, &range);
 
-    vector <int> solution [nfac];
+    //vector <int> solution [nfac];
+    cout << "\n\n GRASP\n";
 
-    Solution(clientes, nfac, range, solution);
+
+    for(int t=0; t < 9;t++){
+      vector <int> solution [nfac];
+      Solution(clientes, nfac, range, solution);
+      //for (int z = 0; z != 20; z++){
+       //fill(solution[z].begin(), solution[z].end(), 0);
+       //solution[z].clear();
+       //cout << solution[z].size() << endl;
+     ///}
+    }
+
+    cout << "\n\n"<< best <<" BEST GRASP \n";
+    cout << "\n\n"<< (media/10) <<" MEDIA GRASP \n";
+
 
     return 0;
 }
